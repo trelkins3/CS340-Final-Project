ba60e99b1fb030530ff4e926ea7466a7ac962393
AdamRuark <adamruark15@gmail.com>
2017-11-27 18:41:35 -0800

    More work on Loggin in and out of sessions

--- FrontEnd/logOut.php
+++ FrontEnd/logOut.php
@@ -34,27 +34,11 @@ session_start();
     </header>
 
     <main>
-        <h2>Log In</h2>
-
-        <div class="form-container">
-            <form action="submitCredentials.php" method="post">
-                <table>
-                    <tr>
-                        <td class="table-desc"><label for="userName">Username:</label></td>
-                        <td><input type="text" name="userName" id="userName"></td>
-                    </tr>
-                    <tr>
-                        <td class="table-desc"><label for="password">Password: </label></td>
-                        <td><input type="password" name="password" id="password"></td>
-                    </tr>
-                </table>
-
-                <input type="submit" value="Login">
-
-            </form>
-        </div>
-
-        <p>Don't have an account? <a href="http://web.engr.oregonstate.edu/~elkinsd/CS340/SatelliteDB/signUp.php">Sign up!</a></p>
+        <h2>You have successfully been logged out</h2>
+        <?php
+            session_unset();
+            session_destroy();
+        ?>
     </main>
     </body>
 </html>
--- FrontEnd/submitCredentials.php
+++ FrontEnd/submitCredentials.php
@@ -11,13 +11,23 @@ session_start();
 	</head>
 <body>
 	<header>
-        <h1><a>SatelliteDB   </a></h1>
+        <h1><a>SatelliteDB</a></h1>
 		<nav>
 			<ul class="navbar-list">
 				<li class="navbar-item"><a href="index.php">Home</a></li>
-				<li class="navbar-item"><a href="logIn.php">Account</a></li>
+				
 				<li class="navbar-item"><a href="satelliteDB.php">Satellite Database</a></li>
 				<li class="navbar-item"><a href="addObject.php">Add Satellite</a></li>
+				<li class="navbar-item"><a href="about.php">About</a></li>
+				<?php
+					if(isset($_SESSION['status'])){
+						echo "<li class=\"navbar-item\"><a href=\"favorites.php\">Favorites</a></li>";
+						echo "<li class=\"navbar-item navbar-right\"><a href=\"logOut.php\">Log Out</a></li>";
+					}
+					else {
+						echo "<li class=\"navbar-item navbar-right\"><a href=\"logIn.php\">Log In</a></li>";
+					}
+				?>
 			</ul>
 		</nav>
     </header>
@@ -52,6 +62,9 @@ session_start();
 		if($dbPassword == $password){
 			$_SESSION['status'] = 'in';
 			$_SESSION['username'] = $userName;
+
+			header("Location: index.php");
+			exit();
 		}
 		else{
 			die('Incorrect Password. <a href="logIn.php"><br><br>Return to Login Page</a>');
